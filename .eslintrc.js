673cd471e97f4897af6e59bd573697fb675493e8
Romain Clement <contact@romain-clement.net>
2020-05-13 12:15:47 +0200

    Update and upgrade dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,13 @@
 module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
-  plugins: [
-    '@typescript-eslint',
-    'prettier'
-  ],
+  plugins: ['@typescript-eslint', 'prettier'],
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     'prettier/@typescript-eslint',
     'prettier',
-    'plugin:prettier/recommended'
-  ]
+    'plugin:prettier/recommended',
+  ],
 }
